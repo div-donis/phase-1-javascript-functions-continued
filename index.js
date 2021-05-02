f0341ad18f93c053580541c09f4a75fe217be929
Donovan <divdonis@gmail.com>
2021-05-01 18:05:11 -0700

    commit tree c2f537cb46f06a6ddd4f6c44210a656d5f508613

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+const mondayWork = function (work = "go to the office"){
+    return `This Monday, I will ${work}.`;
+}
+
+
+function wrapAdjective(wrap = "*"){
+    return function(type = "special"){
+        return `You are ${wrap}${type}${wrap}!`;
+    }
+}
+wrapAdjective()();
